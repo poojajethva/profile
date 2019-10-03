2066062479a7b1057686b1e2e689cc8637aeb2d8
DIRECTI\pooja.j <pooja.j@media.net>
2019-10-04 02:30:15 +0530

    Correcting path again

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ function App() {
           <Router>
             <Menu />    
             <Switch>
-              <Route exact path="/profile/" component={Home} />
-              <Route path="/profile/about" component={About} />
-              <Route path="/profile/skills" component={Skills} />
-              <Route path="/profile/experience" component={Experience} />
-              <Route path="/profile/contact" component={Contact} />
+              <Route exact path="profile" component={Home} />
+              <Route path="profile/about" component={About} />
+              <Route path="profile/skills" component={Skills} />
+              <Route path="profile/experience" component={Experience} />
+              <Route path="profile/contact" component={Contact} />
               <Route path="/" component={Home} />
             </Switch>
           </Router>
