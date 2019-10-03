99ab7bb02adf2e9e43e0d9ed8693a46500d104f5
DIRECTI\pooja.j <pooja.j@media.net>
2019-10-04 02:16:24 +0530

    Updates

---
+++ precache-manifest.9ac9be58d5166418755dd01c948f2ead.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57e49aad31b69f3f1562889ed204bab0",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "c6534374085c47b17932",
+    "url": "/profile/static/css/main.3cbb80c1.chunk.css"
+  },
+  {
+    "revision": "fa15ffec485b8d70db7c",
+    "url": "/profile/static/js/2.477bdf14.chunk.js"
+  },
+  {
+    "revision": "c6534374085c47b17932",
+    "url": "/profile/static/js/main.1d1f81b5.chunk.js"
+  },
+  {
+    "revision": "e9795884afd0465f1414",
+    "url": "/profile/static/js/runtime-main.30999293.js"
+  },
+  {
+    "revision": "eaa9d5e99eb9efc9d620884dd3f38af6",
+    "url": "/profile/static/media/bg.eaa9d5e9.jpg"
+  }
+]);
