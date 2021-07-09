bcb64b33225c5dd07e1f4e0315620fe20a9d5cac
DIRECTI\pooja.j <pooja.j@media.net>
2021-07-09 19:24:21 +0530

    bug fixed

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,34 +5,39 @@ import '../assets/css/home.scss';
 export default class Home extends Component{
     constructor(props) {
         super(props);
-        this.state = { left: 0, top: 0};
-        // this.removeClassAnimation = this.removeClassAnimation.bind(this);
+        this.magicRef = React.createRef();
+        this.state = {
+            values:{ left: 0, top: 0}
+        };
     }
     moveMagicBg(e){
-        let magicWHalf = document.querySelector(".magic").offsetWidth / 2;
+        let magicWHalf = this.magicRef.current;
+        magicWHalf = magicWHalf.offsetWidth / 2;
         this.setState({
-            left: e.pageX - magicWHalf,
-            top: e.pageY - magicWHalf
+            values:{
+                left: e.pageX - magicWHalf,
+                top: e.pageY - magicWHalf
+            }
         })
     }
-
+    
     render(){
         return (
             <div className="home" onMouseMove={this.moveMagicBg.bind(this)}>
-                <div className="magic" style={this.state}></div>
-                <div className="content">
-                    <p className="line line1">
-                        Hi, I’m&nbsp;<span className="sign">&lt;</span>
-                        <span className="txt">Pooja Jethva</span>
-                        <span className="sign"> /&gt;</span>;
-                    </p>
-                    <p className="line line2">
-                        I’m a <span className="txt2">UI Developer</span>.
-                        <span className="cursor">|</span>
-                    </p>
-                </div>
-                <Link className="btnHome" to="/about">View More</Link>
+            <div className="magic" ref={this.magicRef} style={this.state.values}></div>
+            <div className="content">
+            <p className="line line1">
+            Hi, I’m&nbsp;<span className="sign">&lt;</span>
+            <span className="txt">Pooja Jethva</span>
+            <span className="sign"> /&gt;</span>;
+            </p>
+            <p className="line line2">
+            I’m a <span className="txt2">UI Developer</span>.
+            <span className="cursor">|</span>
+            </p>
             </div>
-        )
+            <Link className="btnHome" to="/about">View More</Link>
+            </div>
+            )
+        }
     }
-}
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -5,30 +5,26 @@ import '../assets/css/menu.scss';
 export default class Menu extends Component{
     constructor(props){
         super(props);
+        this.menuRef = React.createRef();
+        this.menuToggleRef = React.createRef();
         this.handleClick = this.handleClick.bind(this);
     }
     toggleMenu(){
-        let menu = document.querySelector(".menu"),
-            toggle = document.querySelector(".menu-toggle");
+        let menu = this.menuRef.current,
+            toggle = this.menuToggleRef.current;
+            console.log(menu, toggle);
 
             toggle.classList.toggle("menu-open");
             menu.classList.toggle("active");
     }
 
     hideMenu(){
-        let menu = document.querySelector(".menu"),
-            toggle = document.querySelector(".menu-toggle");
+        let menu = this.menuRef.current,
+            toggle = this.menuToggleRef.current;
+            console.log(menu, toggle);
 
             toggle.classList.remove("menu-open");
             menu.classList.remove("active");
-    }
-
-    componentWillMount(){
-        document.addEventListener('mousedown', this.handleClick, false);
-    }
-
-    componentWillUnmount(){
-        document.removeEventListener('mousedown', this.handleClick, false);
     }
 
     handleClick(e){
@@ -40,8 +36,8 @@ export default class Menu extends Component{
     render(){
         return (
                 <nav role="navigation" ref={node => this.node = node}>
-                    <div className="menu-toggle" onClick={this.toggleMenu.bind(this)}><span>Menu</span></div>
-                    <ul className="menu">
+                    <div className="menu-toggle" ref={this.menuToggleRef} onClick={this.toggleMenu.bind(this)}><span>Menu</span></div>
+                    <ul className="menu" ref={this.menuRef}>
                         <li><NavLink to="/" exact activeClassName="active" onClick={this.hideMenu.bind(this)}><span>Home</span></NavLink></li>
                         <li><NavLink to="/about" activeClassName="active" onClick={this.hideMenu.bind(this)}><span>About</span></NavLink></li>
                         <li><NavLink to="/skills" activeClassName="active" onClick={this.hideMenu.bind(this)}><span>Skills</span></NavLink></li>
